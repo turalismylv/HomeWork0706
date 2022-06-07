0515395ccfc0e92d6ee5b2852ae1464cbcb9fcbc
turalismylv <turalhi@code.edu.az>
2022-06-07 16:02:03 +0400

    HomeWork0706

---
+++ ConsoleApp1/ConsoleApp1/Extension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class Extension
+    {
+        
+       
+        
+            public static int[] Add(this int[] arr, int num)
+            {
+                Array.Resize(ref arr, arr.Length + 1);
+                arr[arr.Length - 1] = num;
+                return arr;
+            }
+
+
+        }
+
+    }
+
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // aşağıdakı kodu run edəndə 10 cavabı cap olunsun:
+
+
+            int[] arr = new int[0];
+            arr=arr.Add(1);
+           arr= arr.Add(2);
+           arr= arr.Add(3);
+           arr= arr.Add(4);
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
+        }
+        
+        
+        }
+    }
+
+
